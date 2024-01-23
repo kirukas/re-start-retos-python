1d94ddd2334c207570c3d5d11f88a0429542bc7c
enrique hermenegildo <enriqueherme@gmail.com>
2024-01-22 19:30:08 -0600

    my first commit

---
+++ numeros_primos.py
@@ -0,0 +1,14 @@
+""
+Author: LUIS ENRIOQUE HERMENEGIILDO AVENDAÑO
+22-01-2024
+    Display all the prime numbers between 1 to 250.
+    Store the results in a results.txt file.
+   Save the script in (absolute path) .
+""
+def main():
+    print("numeros primos del 1 al 255")
+
+if __name__ == "__main__":
+    main()
+
+
